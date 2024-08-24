670e554c301d2b18ce81a294f66d49d65c2a9f27
Tundephilps <tundephilps@gmail.com>
2024-08-24 13:09:47 +0100

    Complete UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,85 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import Login from "@/screeens/Login";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import SplashScreen from "@/screeens/SplashScreen";
+import Shipment from "@/screeens/TabScreens/Shipment";
+import { Profiler } from "react";
+import Profile from "@/screeens/TabScreens/Profile";
+import Scan from "@/screeens/TabScreens/Scan";
+import Wallet from "@/screeens/TabScreens/Wallet";
+
+type RootStackParamList = {
+  Splash: undefined;
+  Login: undefined;
+  Homepage: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      headerShown: false,
+      tabBarIcon: ({ focused }) => {
+        let iconName;
+
+        if (route.name === "Shipment") {
+          iconName = require("../../assets/images/Tab1.png");
+        } else if (route.name === "Scan") {
+          iconName = require("../../assets/images/Tab2.png");
+        } else if (route.name === "Wallet") {
+          iconName = require("../../assets/images/Tab3.png");
+        } else if (route.name === "Profile") {
+          iconName = require("../../assets/images/Tab4.png");
+        }
+
+        return (
+          <View style={{ alignItems: "center" }}>
+            <Image
+              source={iconName}
+              style={{
+                width: 28,
+                height: 27,
+                margin: 2,
+                tintColor: focused ? "#2F50C1" : "#8e8e8e",
+              }}
+            />
+            <Text style={{ color: focused ? "#2F50C1" : "#8e8e8e" }}>
+              {route.name}
+            </Text>
+          </View>
+        );
+      },
+      tabBarShowLabel: false,
+    })}
+  >
+    <Tab.Screen name="Shipment" component={Shipment} />
+    <Tab.Screen name="Scan" component={Scan} />
+    <Tab.Screen name="Wallet" component={Wallet} />
+
+    <Tab.Screen name="Profile" component={Profile} />
+  </Tab.Navigator>
+);
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <Stack.Navigator
+      initialRouteName="Splash"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Splash" component={SplashScreen} />
+      <Stack.Screen name="Login" component={Login} />
+      {/* <Stack.Screen name="Homepage" component={Shipment} /> */}
+
+      <Stack.Screen name="Homepage" component={TabNavigator} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+const styles = StyleSheet.create({});
